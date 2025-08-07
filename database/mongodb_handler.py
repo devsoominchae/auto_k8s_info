4706677efa3920fe412eec36ee7694c4e47d66ec
Joon Ho Byun <plinko1618@gmail.com>
2025-08-07 17:55:50 +0900

    Fixed changes of db

--- database/mongodb_handler.py
+++ database/mongodb_handler.py
@@ -1,9 +1,8 @@
 from pymongo import MongoClient
 
-# mongodb_handler.py - for connecting to MongoDB and handling log patterns
-#basic layout for now. Allows for updating, and getting error patterns
+# mongodb_handler.py - Handles MongoDB connections and log pattern storage
 class MongoHandler:
-    def __init__(self, uri="mongodb+srv://sasadm:Orion123@logdictionary.ziwndyc.mongodb.net/?retryWrites=true&w=majority&appName=logDictionary", db_name="log_config"):
+    def __init__(self, uri, db_name="log_config"):
         self.client = MongoClient(uri)
         self.db = self.client[db_name]
         self.config = self.db["config"]
