295c8c1a200f828e5193df205868bd845510ed4c
Joon Ho Byun <plinko1618@gmail.com>
2025-08-07 17:55:50 +0900

    Fixed changes of db

--- database/mongdo_test.py
+++ database/mongdo_test.py
@@ -1,18 +1,28 @@
 from mongodb_handler import MongoHandler
-#Simple test to update error patterns in MongoDB - will be added to main.py later
+import getpass
+
+# Prompt user
+username = input("Enter MongoDB username: ")
+password = getpass.getpass("Enter MongoDB password: ")
+
+# Construct URI dynamically using user input
+uri = f"mongodb+srv://{username}:{password}@logdictionary.ziwndyc.mongodb.net/?retryWrites=true&w=majority&appName=logDictionary"
+
+# Define patterns
 conf_patterns = {
     "CAS Control Issues": ["no ready CAS servers", "cas-control is not ready"],
     "Telemetry Warnings": ["OpenTelemetry support not installed"],
     "Readiness Check Failures": [
-            "check \"sas-endpoints-ready\" failed",
-            "no available addresses",
-            "endpoints have no available addresses",
-            "0 available addresses",
-            "failed readiness check"
-        ]
+        'check "sas-endpoints-ready" failed',
+        "no available addresses",
+        "endpoints have no available addresses",
+        "0 available addresses",
+        "failed readiness check"
+    ]
 }
 
-mongo = MongoHandler("mongodb+srv://sasadm:Orion123@logdictionary.ziwndyc.mongodb.net/?retryWrites=true&w=majority&appName=logDictionary")
+# Use dynamic URI to connect
+mongo = MongoHandler(uri)
 mongo.update_error_patterns(conf_patterns)
 
 print("Updated!")
